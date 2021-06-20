d800408d055c647b5da4ce8afe1b491f270d4318
root <3461@holbertonschool.com>
2021-06-19 18:42:33 -0700

    file 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
 
-/*
+/**
  * main - text print
- *
  * Return: 0
  */
-main(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-
 	return (0);
 }
-
