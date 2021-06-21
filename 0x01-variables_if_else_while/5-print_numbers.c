15ee5027fad78a3a57463d146cda388bb2442002
root <3461@holbertonschool.com>
2021-06-21 15:44:24 -0700

    file 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Print numbers
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		printf("%d", x);
+	}
+	putchar('\n');
+	return (0);
+}
