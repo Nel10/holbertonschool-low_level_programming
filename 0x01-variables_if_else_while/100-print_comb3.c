51611a3e7e3ea8f0b8334a988e4ed8b473433304
root <3461@holbertonschool.com>
2021-06-22 08:47:37 -0700

    file 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ * main - two digits
+ * Return: 0
+ */
+int main(void)
+{
+	char x;
+	char y;
+
+	x = '0';
+	y = '1';
+	while (x <= '8')
+	{
+		while (y <= '9')
+		{
+			putchar(x);
+			putchar(y);
+			putchar(',');
+			putchar(' ');
+			y = y + 1;
+		}
+		x = x + 1;
+		y = x + 1;
+	}
+	putchar('\n');
+	return (0);
+}
