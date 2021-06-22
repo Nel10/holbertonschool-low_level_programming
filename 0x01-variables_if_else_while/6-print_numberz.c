fb3b64c7fd3ffa96e9d263fa11e7dababa135320
root <3461@holbertonschool.com>
2021-06-21 18:33:09 -0700

    file 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Print Numberz
+ * Return: 0
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
