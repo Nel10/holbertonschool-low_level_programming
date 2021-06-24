fc772fb3afc8bd4bf47537c5c73f619a83dda94d
root <3461@holbertonschool.com>
2021-06-23 20:53:00 -0700

    alphabet x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+
+/**
+ * print_alphabet_x10 - all alphabet in ten lines
+ *
+ * Return: 0
+ */
+void print_alphabet_x10(void)
+{
+	int x;
+	int j;
+
+	for (x = 0 ; x <= 9 ; x++)
+	{
+		for (j = 'a' ; j <= 'z' ; j++)
+		{
+			_putchar(j);
+		}
+		_putchar('\n');
+	}
+}
