e2eb63f4b1262e8753313a19e88299028cb11cff
root <3461@holbertonschool.com>
2021-06-26 14:23:27 -0700

    file 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ * _isalpha - lowercase and uppercase
+ * @c - variable
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if (c != 59)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
