70321fbe8c7782c6e093cae7fc667388a5b9369c
root <3461@holbertonschool.com>
2021-06-26 21:40:25 -0700

    file7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+
+/**
+ * print_last_digit - print last digit
+ * @n: variable
+ *
+ * Return: 0
+ */
+int print_last_digit(int n)
+{
+	int x;
+
+	x = n % 10;
+	if (n < 0)
+	{
+		_putchar(-x + '0');
+		return (-x);
+	}
+	else
+	{
+		_putchar(x + '0');
+		return (x);
+	}
+}
