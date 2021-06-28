f7cef226512992aa983540e9cbe7833949813b5d
root <3461@holbertonschool.com>
2021-06-27 19:57:01 -0700

    File10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * add - sum of two numbers
+ * @a: variable
+ * @b: variable
+ *
+ * Return: 0
+ */
+int add(int a, int b)
+{
+	int n;
+
+	n = a + b;
+	return (n);
+}
