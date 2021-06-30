b8d8fcd4e1fa71038d55c408c1e7de011f512ff7
root <3461@holbertonschool.com>
2021-06-29 20:41:13 -0700

    file 4

--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -1,6 +1,17 @@
+#ifndef HOLBERTON_H
+
+#define HOLBERTON_H
+
+
+
+#include <stdio.h>
+
+#include <stdlib.h>
+
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
 int _putchar(char c);
 void print_rev(char *s);
+#endif
