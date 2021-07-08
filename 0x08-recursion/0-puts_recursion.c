3ac6f65fc71a76bcb490bb0a90c6832a9d31a6ba
root <3461@holbertonschool.com>
2021-07-07 20:04:02 -0700

    file 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _puts_recursion - prints a string
+ * @s: pointer
+ * Return: 0
+ */
+void _puts_recursion(char *s)
+{
+	int x;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		_putchar(s[x]);
+	}
+}
