1818bc1c2a8d0f0b2f93740cd0a7d128c48f9809
root <3461@holbertonschool.com>
2021-07-08 20:58:56 -0700

    file 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,22 +1,19 @@
 #include "holberton.h"
 
 /**
+ * factorial - Returns the factorial of a given number.
+ * @n: variable
+ * Return: 0
  */
 int factorial(int n)
 {
 	int x;
 
 	if (n == 0)
-	{
 		x = 1;
-	}
 	else if (n < 0)
-	{
 		x = -1;
-	}
 	else
-	{
 		x = n * factorial(n - 1);
-	}
 	return (x);
 }
