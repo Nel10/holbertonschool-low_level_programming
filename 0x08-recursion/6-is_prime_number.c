d4c1cc39c8f37824bd0913e733bb9a1645685786
root <3461@holbertonschool.com>
2021-07-08 21:33:03 -0700

    file 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+
+/**
+ * num_prime - the input integer is a prime number 
+ * @x: variable
+ * @y: variable
+ * Return: 0
+ */
+int num_prime(int x, int y)
+{
+	if ((x % y == 0 && y < x) || x < 2)
+		return (0);
+	else if (x % y == 0 && y == x)
+		return (1);
+	else
+		return (num_prime(x, y + 1));
+}
+/**
+ * is_prime_number - function that return 1 or 0
+ * @n: variable
+ * Return: 0
+ */
+int is_prime_number(int n)
+{
+	return (num_prime(n, 2));
+}
