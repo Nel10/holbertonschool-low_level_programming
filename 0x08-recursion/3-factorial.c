4f00cd4be78b2426722f2e716c5ffd5cdcca9c3e
root <3461@holbertonschool.com>
2021-07-08 20:44:03 -0700

    file 3

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+
+/**
+ */
+int factorial(int n)
+{
+	int x;
+
+	if (n == 0)
+	{
+		x = 1;
+	}
+	else if (n < 0)
+	{
+		x = -1;
+	}
+	else
+	{
+		x = n * factorial(n - 1);
+	}
+	return (x);
+}
