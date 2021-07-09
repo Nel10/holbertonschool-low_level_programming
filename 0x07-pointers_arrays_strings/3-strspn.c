f1fd993d8d6eddfcd49275903f4f31fbfbe92d57
root <3461@holbertonschool.com>
2021-07-09 15:24:36 -0700

    file 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+
+/**
+ * _strspn - the length of a prefix substring.
+ * @s: pointer
+ * @accept: pointer
+ * Return: 0
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int x = 0, i, j = 0;
+
+	while (accept[x])
+	{
+		i = 0;
+		while (s[i] != 32)
+		{
+			if (accept[x] != s[i])
+			{
+				j++;
+			}
+			i++;
+		}
+		x++;
+	}
+	return (j);
+}
+
