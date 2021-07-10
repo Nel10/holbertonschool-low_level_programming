b18662859829a58823bbf252423d993e17e47779
root <3461@holbertonschool.com>
2021-07-09 18:21:57 -0700

    file 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the number of arguments passed into it.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: EXIT_SUCCESS
+ */
+int main(int argc, char *argv[])
+{
+	printf("&d\n", argc - 1);
+	exit(EXIT_SUCCESS);
+}
