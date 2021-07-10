93f161b1d96f2e32222972b32e23667c06aa1a26
root <3461@holbertonschool.com>
2021-07-09 18:45:24 -0700

    file 3

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main -prints all arguments it receives.
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x;
+
+	for (x = 0; x < argc; x++)
+		printf("%s\n", argv[1])
+			return (0);
+}
