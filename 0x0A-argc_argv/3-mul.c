7a390616ab8079af8321002fd8ac0a4eafcb878e
root <3461@holbertonschool.com>
2021-07-09 19:40:50 -0700

    file 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - multiplies two numbers.
+ * @argc: int
+ * @argv: char
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	int x, y, z;
+
+	if (argc != 3)
+	{
+		printf("error\n");
+		return (1);
+	}
+	else
+	{
+		x = atoi(argv[1]);
+		y = atoi(argv[2]);
+		z = x * y;
+		printf("%d\n", z);
+	}
+	return (0);
+}
+
