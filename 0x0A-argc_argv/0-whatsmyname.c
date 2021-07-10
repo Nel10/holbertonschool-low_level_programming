1366955d31bad5027ea716f020c149e2d0e4ecb8
root <3461@holbertonschool.com>
2021-07-09 18:12:06 -0700

    file 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints its name
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: the name, EXIT_SUCCESS
+ */
+int main(int argc, char* argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	exit(EXIT_SUCCESS);
+}	
