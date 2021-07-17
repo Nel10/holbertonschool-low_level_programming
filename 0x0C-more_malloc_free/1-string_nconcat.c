74c183773f70a1ae500fa568dad473b2e04f0a3b
root <3461@holbertonschool.com>
2021-07-16 19:24:03 -0700

    file 1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,56 +2,37 @@
 #include <stdlib.h>
 
 /**
+ * string_nconcat - concatenates two strings.
+ * @s1: pointer
+ * @s2: pointer
+ * @n: variable
+ * Return: 0
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	char *strDup;
+	int i;
+	unsigned int j;
 
-	  int i;
-
-	    unsigned j;
-
-
-
-	      if (!s1 && !s2)
-
-		          return (NULL);
-
-	        i = 0;
-
-		  while(s1[i] != '\0')
-
-			      i++;
-
-		    strDup = malloc(sizeof(char) * (i + n + 1));
-
-		      if (strDup == NULL)
-
-			      		return (NULL);
-
-		      	i = j = 0;
-
-			  while (s1[i] != '\0')
-
-				  	{
-
-								strDup[i] = s1[i];
-
-										i++;
-
-											}
-
-			  	while (j < n && s2[j] != '\0')
-
-						{
-
-									strDup[i] = s2[j];
-
-											i++, j++;
-
-												}
-
-					strDup[i] = '\0';
-
-						return (strDup);
+	if (!s1 && !s2)
+		return (NULL);
+	i = 0;
+	while (s1[i] != '\0')
+		i++;
+	strDup = malloc(sizeof(char) * (i + n + 1));
+	if (strDup == NULL)
+		return (NULL);
+	i = j = 0;
+	while (s1[i] != '\0')
+	{
+		strDup[i] = s1[i];
+		i++;
+	}
+	while (j < n && s2[j] != '\0')
+	{
+		strDup[i] = s2[j];
+		i++, j++;
+	}
+	strDup[i] = '\0';
+	return (strDup);
 }
