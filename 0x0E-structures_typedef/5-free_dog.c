d0aa228ef1828395163a7c669d90e3ddd1a1e49a
root <3461@holbertonschool.com>
2021-07-20 18:01:25 -0700

    file 5

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,20 @@
+
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * free_dog -  function that frees dogs.
+ * @d: pointer
+ * Return: 0
+ */
+void free_dog(dog_t *d)
+{
+	dog_t *my_dog = d;
+
+	if (d)
+	{
+		free(my_dog->name);
+		free(my_dog->owner);
+		free(my_dog);
+	}
+}
