a50545b3839badf91f9480a282c09753fabeea42
root <3461@holbertonschool.com>
2021-07-21 16:53:48 -0700

    file 0

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints a name.
+ * @name: pointer of string
+ * @f: funtion pointer
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,1 @@
+im learn about function_pointers
