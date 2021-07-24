3f815f6e4e33ad4efff9192394e18beba27b7c44
root <3461@holbertonschool.com>
2021-07-23 23:47:35 -0700

    file 2

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -14,7 +14,6 @@ typedef struct impre
 	char *t;
 	void (*f)(va_list);
 } impresion;
-int _putchar(char c);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 int sum_them_all(const unsigned int n, ...);
