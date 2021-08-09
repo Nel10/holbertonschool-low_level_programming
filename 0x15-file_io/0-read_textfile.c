bcce0077ab447b306cc51e60e84bddbf40a8b6a5
root <3461@holbertonschool.com>
2021-08-09 12:56:49 -0700

    file 0

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints
+ * @filename: file to read
+ * @letters: content
+ * Return: w
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t o, r, w;
+	char *buf;
+
+	if (filename == NULL)
+	{
+		return (0);
+	}
+     buf = malloc(sizeof(char) * letters);
+     if (buf == NULL)
+     {
+         return (0);
+     }
+     o = open(filename, O_RDONLY);
+     r = read(o, buf, letters);
+     w = write(1, buf, r);
+     if(o == -1 || r == -1 || w == -1)
+     {
+	     return (0);
+     }
+     free(buf);
+     close(o);
+     return (w);
+}
