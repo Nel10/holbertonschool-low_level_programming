696fb38ff2aef150acaac312f895d956d3420f53
root <3461@holbertonschool.com>
2021-08-09 13:02:26 -0700

    file 0 fixed

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -15,19 +15,19 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	{
 		return (0);
 	}
-     buf = malloc(sizeof(char) * letters);
-     if (buf == NULL)
-     {
-         return (0);
-     }
-     o = open(filename, O_RDONLY);
-     r = read(o, buf, letters);
-     w = write(1, buf, r);
-     if(o == -1 || r == -1 || w == -1)
-     {
-	     return (0);
-     }
-     free(buf);
-     close(o);
-     return (w);
+	buf = malloc(sizeof(char) * letters);
+	if (buf == NULL)
+	{
+		return (0);
+	}
+	o = open(filename, O_RDONLY);
+	r = read(o, buf, letters);
+	w = write(1, buf, r);
+	if (o == -1 || r == -1 || w == -1)
+	{
+		return (0);
+	}
+	free(buf);
+	close(o);
+	return (w);
 }
