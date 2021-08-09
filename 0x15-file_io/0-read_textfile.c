2771d2e4ed2642f106069ed635285f22c279969f
root <3461@holbertonschool.com>
2021-08-09 13:26:34 -0700

    file 0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,14 +11,14 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t o, r;
 	char *buf;
 
-	buf = malloc(sizeof(char) * letters);
+	buf = malloc(sizeof(char *) * letters);
 	if (!buf | !filename)
 	{
 		return (0);
 	}
 	o = open(filename, O_RDONLY);
 	r = read(o, buf, letters);
-	write(1, buf, r);
+	write(STDOUT_FILENO, buf, r);
 	if (o == -1)
 	{
 		return (0);
