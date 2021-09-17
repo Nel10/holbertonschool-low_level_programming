050dad1cd396b49487ba2909f1908da97067d455
Nel10 <3461@holbertonschool.com>
2021-09-17 18:19:58 -0500

    file 2

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -20,9 +20,12 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = *head;
 	new_node->prev = NULL;
+	new_node->next = *head;
 	*head = new_node;
-
-	return (*head);
+	if (new_node->next != NULL)
+	{
+		new_node->next->prev = new_node;
+	}
+	return (new_node);
 }
