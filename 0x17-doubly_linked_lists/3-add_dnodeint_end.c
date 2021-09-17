9aec4a1886d372fa5f49632ecdd946e75f961185
Nel10 <3461@holbertonschool.com>
2021-09-17 17:07:48 -0500

    file fixed

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -21,7 +21,7 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new_node->n = n;
-	new_node->next = NULL;
+	new_node->prev = NULL;
 	if (*head == NULL)
 	{
 		new_node->prev = NULL;
