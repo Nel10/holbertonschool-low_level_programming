7f305469433db82e9a7c0f6100af971db85a32c6
Nel10 <3461@holbertonschool.com>
2021-09-17 21:20:41 -0500

    file 7 fixed

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,47 +1,50 @@
 #include "lists.h"
 
 /**
- * insert_dnodeint_at_index - inserts a new node at a given position.
- * @h: pointer of struct head
+ * insert_dnodeint_at_index - insert node
+ * @h: head
  * @idx: index
  * @n: node
- * Return: new_node
+ * Return: the address of the new node, or NULL if it failed
  */
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 {
-	dlistint_t *new_node, *next, *current;
+	dlistint_t *new_node, *previous, *current;
 	unsigned int i = 0;
 
-	if (h == NULL)
-		return (NULL);
-	if (idx != 0)
+	if (*h == NULL && idx != 0)
 	{
-		current = *h;
-		while (i < idx - 1 && current != NULL)
-			current = current->next;
-		if (current == NULL)
-		{
-			return (NULL);
-		}
+		return (NULL);
 	}
+	if (idx == 0)
+		return (add_dnodeint(h, n));
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 		return (NULL);
 	new_node->n = n;
-	if (idx == 0)
+	new_node->prev = NULL;
+	new_node->next = NULL;
+	current = *h;
+	previous = NULL;
+	while (current != NULL)
 	{
-		next = *h;
-		*h = new_node;
-		new_node->prev = NULL;
+		if (i == idx)
+		{
+			previous->next = new_node;
+			new_node->prev = previous;
+			new_node->next = current;
+			current->prev = new_node;
+			return (new_node);
+		}
+		if ((i == idx - 1) && (current->next == NULL))
+		{
+			current->next = new_node;
+			new_node->prev = current;
+			return (new_node);
+		}
+		previous = current;
+		current = previous->next;
+		i++;
 	}
-	else
-	{
-		new_node->prev = current;
-		next = current->next;
-		current->next = new_node;
-	}
-	new_node->next = next;
-	if (new_node != NULL)
-		new_node->next->prev = new_node;
 	return (new_node);
 }
