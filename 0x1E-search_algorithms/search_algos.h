d5293ab4a461e02acac92641bee9a9a5a41b7be2
Nel10 <3461@holbertonschool.com>
2022-01-18 15:33:17 -0500

    header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -2,8 +2,9 @@
 #define SEARCH_ALGORITMOS
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-
+int jump_search(int *array, size_t size, int value);
 #endif /*SEARCH_ALGORITMOS*/
