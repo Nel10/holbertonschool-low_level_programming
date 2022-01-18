ab4a7109d6fec1c7a91023682fe25d60766e6fb7
Nel10 <3461@holbertonschool.com>
2022-01-17 22:21:42 -0500

    update

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,38 +11,24 @@ int binary_search(int *array, size_t size, int value)
 {
 	size_t l = 0, i;
 	size_t r = size - 1, mid;
-	int flag = 0;
 
 	if (array == NULL || size == 0)
 		return (-1);
-	while (l < r)
+	while (l <= r)
 	{
 		printf("Searching in array: ");
-		i = l;
-		flag = 0;
-		while (i <= r)
+		for (i = l; i < r; i++)
 		{
-			if (flag == 1)
-			{
-				printf(", ");
-			}
-			printf("%d", array[i]);
-			flag = 1;
-			i++;
+			printf("%d, ", array[i]);
 		}
-		printf("\n");
+		printf("%d\n", array[i]);
 		mid = (l + r) / 2;
-		if (value == array[mid])
-			return (mid);
-		else if (value < array[mid])
-		{
-			r = mid - 1;
-		}
-		else
-		{
+		if (array[mid] < value)
 			l = mid + 1;
-		}
+		else if (value < array[mid])
+			r = mid - 1;
+		else
+			return (mid);
 	}
-	printf("Searching in array: %d\n", array[mid + 1]);
 	return (-1);
 }
